2d502f80682758c992f0ebcd8a96bb939431065f
prasant8717 <prasant8717@gmail.com>
2024-10-24 08:47:44 +0530

    Create MinAbsDiffBST.cpp

---
+++ MinAbsDiffBST.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getMinimumDifference(TreeNode* root) {
+        int out = INT_MAX;
+        int prv = -1;
+        stack<TreeNode*> stk;
+        while (root != nullptr || !stk.empty()) {
+            while (root != nullptr) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top(), stk.pop();
+            if (prv >= 0)
+            out = min(out, root->val - prv);
+            prv = root->val;
+            root = root->right;
+        }
+        return out;
+    }
+};
