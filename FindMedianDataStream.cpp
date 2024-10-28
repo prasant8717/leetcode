4d96b6c1854bbcd9e2ef11dc6697e28c3d6853ea
prasant8717 <prasant8717@gmail.com>
2024-10-28 21:38:24 +0530

    Create FindMedianDataStream.cpp

---
+++ FindMedianDataStream.cpp
@@ -0,0 +1,33 @@
+class MedianFinder {
+public:
+    priority_queue<int> pq1;
+    priority_queue<int, vector<int>, greater<>> pq2;
+
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if (pq1.empty() || num <= pq1.top())
+            pq1.push(num);
+        else
+            pq2.push(num);
+        if (pq1.size() < pq2.size())
+            pq1.push(pq2.top()), pq2.pop();
+        else if (pq1.size() - pq2.size() > 1)
+            pq2.push(pq1.top()), pq1.pop();
+    }
+    
+    double findMedian() {
+        if (pq1.size() == pq2.size())
+            return (pq1.top() + pq2.top()) / 2.0;
+        return pq1.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
