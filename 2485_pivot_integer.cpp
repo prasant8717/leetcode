c63bffd5858397c8d2923df150d50ef959323347
Prasant Behera <prasant8717@gmail.com>
2025-06-22 14:57:19 +0530

    Create 2485_pivot_integer.cpp

---
+++ 2485_pivot_integer.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        for (int i = 1; i <= n; ++ i) {
+            if ((1 + i) * i == (i + n) * (n - i + 1)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
