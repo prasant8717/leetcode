f6ffe24c72f1e83880bb47681af190bc429d8eae
Prasant Behera <prasant8717@gmail.com>
2025-07-02 21:05:20 +0530

    Create 2200_findKDistantIndices.cpp

---
+++ 2200_findKDistantIndices.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> out;
+        int n = nums.size();
+        for (int i = 0; i < n; ++ i)
+            for (int j = 0; j < n; ++ j)
+                if ((abs(i - j) <= k) && (nums[j] == key)) {
+                    out.push_back(i);
+                    break;
+                }
+        return out;
+    }
+};
